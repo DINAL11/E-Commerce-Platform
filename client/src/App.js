505a4f44b44d07c4833314629181a83953fec907
Dinal <79514410+DINAL11@users.noreply.github.com>
2025-05-01 23:03:25 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import ProductPage from "./components/ProductPage";
+import CartPage from "./components/CartPage";
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
